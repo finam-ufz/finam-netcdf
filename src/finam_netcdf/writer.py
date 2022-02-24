2b070139f3c3e8d64e34f38eb6113b6b6cb0fb9f
Martin Lange <martin.lange@ufz.de>
2022-02-24 12:13:55 +0100

    move extraction of variables and dimensions to separate function, simplify loop

--- src/finam_netcdf/writer.py
+++ src/finam_netcdf/writer.py
@@ -54,84 +54,15 @@ class NetCdfTimedWriter(ATimeComponent):
     def validate(self):
         super().validate()
 
-        variables = {}
-        x_dims = {}
-        y_dims = {}
+        variables, coords = extract_vars_dims(self.inputs, self._input_dict, self.time)
 
-        for (name, inp), (_n, layer) in zip(
-            self.inputs.items(), self._input_dict.items()
-        ):
-            data = inp.pull_data(self.time)
+        coords = dict({self.time_var: np.ndarray(0, dtype="datetime64[ns]")}, **coords)
 
-            if not isinstance(data, Grid):
-                raise ValueError(
-                    "Only data of type `Grid` can be added to NetCDF files."
-                )
-
-            if layer.var in variables:
-                raise ValueError("Duplicate variable %s." % (layer.var,))
-
-            variables[layer.var] = layer, data.dtype
-
-            if layer.x in y_dims:
-                raise ValueError(
-                    "Y dimension '%s' is already defined."
-                    "Can't be redefined as X dimension by input '%s'" % (layer.x, name)
-                )
-            if layer.y in x_dims:
-                raise ValueError(
-                    "X dimension '%s' is already defined."
-                    "Can't be redefined as Y dimension by input '%s'" % (layer.y, name)
-                )
-
-            if layer.x in x_dims:
-                spec = x_dims[layer.x]
-                if (
-                    spec.xll != data.spec.xll
-                    or spec.cell_size != data.spec.cell_size
-                    or spec.ncols != data.spec.ncols
-                ):
-                    raise ValueError(
-                        "X dimension '%s' is already defined."
-                        "Definition differs from data provided by input '%s'"
-                        % (layer.x, name)
-                    )
-            else:
-                x_dims[layer.x] = data.spec
-
-            if layer.y in y_dims:
-                spec = y_dims[layer.y]
-                if (
-                    spec.yll != data.spec.yll
-                    or spec.cell_size != data.spec.cell_size
-                    or spec.nrows != data.spec.nrows
-                ):
-                    raise ValueError(
-                        "Y dimension '%s' is already defined."
-                        "Definition differs from data provided by input '%s'"
-                        % (layer.y, name)
-                    )
-            else:
-                y_dims[layer.y] = data.spec
-
-        x_dims = {
-            name: np.arange(spec.ncols) * spec.cell_size
-            + (spec.xll + 0.5 * spec.cell_size)
-            for name, spec in x_dims.items()
-        }
-        y_dims = {
-            name: np.flip(
-                np.arange(spec.nrows) * spec.cell_size
-                + (spec.yll + 0.5 * spec.cell_size)
-            )
-            for name, spec in y_dims.items()
-        }
-        coords = dict(
-            {self.time_var: np.ndarray(0, dtype="datetime64[ns]")}, **x_dims, **y_dims
-        )
         self.data_arrays = {
             layer.var: xr.DataArray(
-                np.ndarray((0, data.spec.nrows, data.spec.ncols), dtype=dtype),
+                np.ndarray(
+                    (0, coords[layer.y].shape[0], coords[layer.x].shape[0]), dtype=dtype
+                ),
                 coords=[coords[self.time_var], coords[layer.y], coords[layer.x]],
                 dims=[self.time_var, layer.y, layer.x],
             )
@@ -143,9 +74,8 @@ class NetCdfTimedWriter(ATimeComponent):
     def update(self):
         super().update()
 
-        for (name, inp), (_n, layer) in zip(
-            self.inputs.items(), self._input_dict.items()
-        ):
+        for name, inp in self.inputs.items():
+            layer = self._input_dict[name]
             data = inp.pull_data(self.time)
 
             if not isinstance(data, Grid):
@@ -178,3 +108,76 @@ class NetCdfTimedWriter(ATimeComponent):
         dataset.close()
 
         self._status = ComponentStatus.FINALIZED
+
+
+def extract_vars_dims(inputs, layers, t0):
+    variables = {}
+    x_dims = {}
+    y_dims = {}
+
+    for name, inp in inputs.items():
+        layer = layers[name]
+        data = inp.pull_data(t0)
+
+        if not isinstance(data, Grid):
+            raise ValueError("Only data of type `Grid` can be added to NetCDF files.")
+
+        if layer.var in variables:
+            raise ValueError("Duplicate variable %s." % (layer.var,))
+
+        variables[layer.var] = layer, data.dtype
+
+        if layer.x in y_dims:
+            raise ValueError(
+                "Y dimension '%s' is already defined."
+                "Can't be redefined as X dimension by input '%s'" % (layer.x, name)
+            )
+        if layer.y in x_dims:
+            raise ValueError(
+                "X dimension '%s' is already defined."
+                "Can't be redefined as Y dimension by input '%s'" % (layer.y, name)
+            )
+
+        if layer.x in x_dims:
+            spec = x_dims[layer.x]
+            if (
+                spec.xll != data.spec.xll
+                or spec.cell_size != data.spec.cell_size
+                or spec.ncols != data.spec.ncols
+            ):
+                raise ValueError(
+                    "X dimension '%s' is already defined."
+                    "Definition differs from data provided by input '%s'"
+                    % (layer.x, name)
+                )
+        else:
+            x_dims[layer.x] = data.spec
+
+        if layer.y in y_dims:
+            spec = y_dims[layer.y]
+            if (
+                spec.yll != data.spec.yll
+                or spec.cell_size != data.spec.cell_size
+                or spec.nrows != data.spec.nrows
+            ):
+                raise ValueError(
+                    "Y dimension '%s' is already defined."
+                    "Definition differs from data provided by input '%s'"
+                    % (layer.y, name)
+                )
+        else:
+            y_dims[layer.y] = data.spec
+
+    x_dims = {
+        name: np.arange(spec.ncols) * spec.cell_size + (spec.xll + 0.5 * spec.cell_size)
+        for name, spec in x_dims.items()
+    }
+    y_dims = {
+        name: np.flip(
+            np.arange(spec.nrows) * spec.cell_size + (spec.yll + 0.5 * spec.cell_size)
+        )
+        for name, spec in y_dims.items()
+    }
+    coords = dict(x_dims, **y_dims)
+
+    return variables, coords
