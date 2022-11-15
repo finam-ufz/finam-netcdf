eae364b7444dc462a272d80a3bc42cf4a9c1f6c2
Martin Lange <martin.lange@ufz.de>
2022-11-15 13:15:15 +0100

    fix moved intersphinx inventories

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -101,8 +101,8 @@ html_theme_options = {
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    "Python": ("https://docs.python.org/", None),
-    "NumPy": ("http://docs.scipy.org/doc/numpy/", None),
+    "Python": ("https://docs.python.org/3/", None),
+    "NumPy": ("https://numpy.org/doc/stable/", None),
     "xarray": ("https://docs.xarray.dev/en/stable/", None),
     "pytest": ("https://docs.pytest.org/en/7.1.x/", None),
     "finam": ("https://finam.pages.ufz.de/finam/", None),
