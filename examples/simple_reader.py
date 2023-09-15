1a2e8596d879147902b8254c84fe4b4d748b58f7
Sebastian Müller <mueller.seb@posteo.de>
2023-09-15 12:41:19 +0200

    update examples

--- examples/simple_reader.py
+++ examples/simple_reader.py
@@ -4,23 +4,18 @@ import finam as fm
 import matplotlib.pyplot as plt
 from finam_plot import ContourPlot
 
-from finam_netcdf import Layer, NetCdfStaticReader
+from finam_netcdf import NetCdfStaticReader, Variable
 
-if __name__ == "__main__":
-    path = "tests/data/lai.nc"
+path = "tests/data/lai.nc"
 
-    reader = NetCdfStaticReader(
-        path,
-        {"LAI": Layer(var="lai", xyz=("lon", "lat"), fixed={"time": 0})},
-    )
+reader = NetCdfStaticReader(path, [Variable("lai", io_name="LAI", slices={"time": 0})])
+viewer = ContourPlot()
 
-    viewer = ContourPlot()
+composition = fm.Composition([reader, viewer])
+composition.initialize()
 
-    composition = fm.Composition([reader, viewer])
-    composition.initialize()
+reader.outputs["LAI"] >> viewer.inputs["Grid"]
 
-    reader.outputs["LAI"] >> viewer.inputs["Grid"]
+composition.run()
 
-    composition.run()
-
-    plt.show(block=True)
+plt.show(block=True)
